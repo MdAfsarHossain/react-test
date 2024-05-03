28dd0bf086977310aac2c235c7a58ec32af7062f
MdAfsarHossain <afsar291722@gmail.com>
2024-05-03 21:04:25 +0100

    Code updated

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
         element: <Contact />,
       },
       {
-        path: '/product',
+        path: 'product',
         element: <Product />,
       },
     ]
